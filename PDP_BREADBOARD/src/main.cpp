54f83a2355416e34dfb6c63cf369db87e3183452
Marin29 <70522474+Marin29@users.noreply.github.com>
2023-01-10 11:43:18 +0100

    fin

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -2,9 +2,10 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-#define DHTPIN 2 // Digital pin connected to the DHT sensor
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Define the pins that we will use
+#define SENSOR 33
+#define LED 26
 
 // Uncomment the type of sensor in use:
 // #define DHTTYPE    DHT11     // DHT 11
@@ -14,12 +15,10 @@
 // See guide for details on sensor wiring and usage:
 //   https://learn.adafruit.com/dht/overview
 
-DHT_Unified dht(DHTPIN, DHTTYPE);
+DHT_Unified dht(SENSOR, DHTTYPE);
 uint32_t delayMS;
 
-// Define the pins that we will use
-#define SENSOR 33
-#define LED 26
+
 
 void setup()
 {
