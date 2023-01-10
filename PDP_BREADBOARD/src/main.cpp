a579e0a0b0da4b6f7d0cfeb01deb2dca6d162e32
Marin29 <70522474+Marin29@users.noreply.github.com>
2023-01-10 11:56:46 +0100

    FIN

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -2,9 +2,9 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-#define DHTPIN 2 // Digital pin connected to the DHT sensor
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
+// Define the pins that we will use
+#define SENSOR 33
+#define LED 26
 
 // Uncomment the type of sensor in use:
 // #define DHTTYPE    DHT11     // DHT 11
@@ -14,12 +14,8 @@
 // See guide for details on sensor wiring and usage:
 //   https://learn.adafruit.com/dht/overview
 
-DHT_Unified dht(DHTPIN, DHTTYPE);
+DHT_Unified dht(SENSOR, DHTTYPE);
 uint32_t delayMS;
-
-// Define the pins that we will use
-#define SENSOR 33
-#define LED 26
 
 void setup()
 {
@@ -71,12 +67,9 @@ void setup()
   Serial.println(F("------------------------------------"));
   // Set delay between sensor readings based on sensor details.
   delayMS = sensor.min_delay / 1000;
-}
 
-void loop()
-{
   // Delay between measurements.
-  delay(5000);
+
   // Get temperature event and print its value.
   sensors_event_t event;
   dht.temperature().getEvent(&event);
@@ -102,4 +95,10 @@ void loop()
     Serial.print(event.relative_humidity);
     Serial.println(F("%"));
   }
+  esp_sleep_enable_timer_wakeup(5000000);
+  esp_deep_sleep_start();
+}
+
+void loop()
+{
 }
